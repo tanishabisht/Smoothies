15f8e15055a563267e9c664d95eca073393cd868
Tanisha Bisht <tanishabisht@Tanishas-MBP-2.lan>
2024-04-18 20:33:11 -0400

    modify home page + result page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+    const navigate = useNavigate();
+
+    const onClickLearnBtn = () => {
+        navigate(`/lesson/1`);
+    }
+
+    const onClickQuizBtn = () => {
+        navigate(`/quiz/1`);
+    }
+
     return (
-        <div>
-            <h1>Welcome to SmoothiesMania!!</h1>
-            {/* Add buttons for Start Learning and Assess */}
-            <h2>Ditch the Powder: Go Natural with Your Protein!</h2>
-            <p>Why spend more on protein powder when you can get all you need from nature? Not only can excessive intake of protein powders be costly, but it might also lead you to miss out on the whole foods essential for a balanced diet. What's more, too much protein powder could stress your kidneys and liver.</p>
-            <div style={{ textAlign: 'center', marginTop: '20px' }}>
-                <button onClick={() => alert('Start Learning button clicked')}>Start Learning</button>
-                <button onClick={() => alert('Assess button clicked')}>Assess</button>
+        <div className='home_container'>
+            <div className='home_padding'>
+                <h1>Welcome to SmoothiesMania!!</h1>
+                <h2>Ditch the Powder: Go Natural with Your Protein!</h2>
+                <p>Why spend more on protein powder when you can get all you need from nature? Not only can excessive intake of protein powders be costly, but it might also lead you to miss out on the whole foods essential for a balanced diet. What's more, too much protein powder could stress your kidneys and liver.</p>
+                <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                    <button className='main_btn' onClick={onClickLearnBtn}>Teach Me</button>
+                    <button className='secondary_btn' onClick={onClickQuizBtn}>Quiz Me</button>
+                </div>
             </div>
         </div>
     );
--- src/pages/QuizResult.js
+++ src/pages/QuizResult.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const QuizResultPage = () => {
+    const navigate = useNavigate();
+
+    const onClickLearnBtn = () => {
+        navigate(`/lesson/1`);
+    }
+
+    const onClickQuizBtn = () => {
+        navigate(`/quiz/1`);
+    }
+
     return (
-        <div>Quiz Result Page</div>
+        <div className='home_container'>
+            <div className='home_padding'>
+                <h1>Congratulationsss!!!</h1>
+                <h2>You now know Protein Power Ingredients for your smoothie!</h2>
+                <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                    <button className='main_btn' onClick={onClickLearnBtn}>Teach Me</button>
+                    <button className='secondary_btn' onClick={onClickQuizBtn}>Quiz Me Again</button>
+                </div>
+            </div>
+        </div>
     );
 }
 
